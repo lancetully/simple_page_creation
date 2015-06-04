821bfaf0d68e854f3bafdc64a07768dbcefd542d
lancetully <ltully@omniupdate.com>
2015-06-04 07:59:01 -0700

    transitioning functional code into modular

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #simple_page_creation
 
+1. have nodejs installed
+1. open a terminal
+1. navigate to the directory where simple_page_creation is located
+1. run `npm install`
+1. clean the output folder `rm -R output/*`
+1. run the app `npm start`
--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var fs = require('fs'),
 // using the subjects array, create files for each subject page.
 // using *Sync methods to avoid callbacks for this simple script
 
-// subjects is the array of subject IDs. 
+// subjects is the array of subject IDs. for these pages, we only need this array
 var subjects = ["ACCT", "AGRI", "AGEC", "AGPS", "ASDC", "ANSC", "ANTH", "ARCH", "AUTO", "BSET", "BIOL", "BLCT", "BUAD", "CHEM", "CIVL", "COMP", "CISY", "CTRP", "CJUS", "DGMA", "DCAD", "ECON", "EDUC", "ELET", "ELTR", "EMET", "ENGR", "ESOL", "ENVR", "FILM", "FNAT", "FDSR", "FRSC", "GEOL", "GLST", "HPED", "MEDR", "HLTH", "HIST", "HUSR", "DSGN", "ITAL", "JAPN", "LITR", "MATT", "MKTG", "MATH", "MECH", "NASC", "NURS", "PHIL", "PHYS", "PLSC", "PSYC", "SOCI", "SPAN", "SPCH", "SPMG", "TMGT", "VETS", "WELD"],
+	// read our template files into local vars
 	tmpl_props = fs.readFileSync("templates/_props.mst","utf8"),
 	tmpl_index = fs.readFileSync("templates/index.mst","utf8");
 
@@ -32,12 +33,27 @@ subjects.forEach(function(item){
 		// in this case, we have no template for the _nav.inc. they will be blank on creation
 		fs.writeFileSync(navpath);
 
-		console.log(path + " files written");
+		// console.log(path + " files written");
 	})
 	
 })
 
-// debug callback
+// callback placeholder
 function callback(data){
 	console.log("default debug output:: ", data)
 }
+
+// @TODO add config object and export a simple_page_creation function
+var config = {
+	// data is an array of objects, each containing page data neede in templates
+	data : subjects.map(function(item,index){var itemObj = {}; itemObj.courseID = item.toLowerCase(); return itemObj;}),
+	paths : [{templates : "templates"},{output:"output"},{nav:""}],
+	templates : [{name:"props", file:"_props.mst"},{name:"index.mst", file:"index.mst"}],
+	output_extension :"pcf"
+}
+
+for (var item in config.data){
+	console.log("item # : " ,config.data[item].courseID)
+
+}
+
